ccad29f80deeddd3486006a4b90bd263109f81b3
xgbt <1027581514@qq.com>
2023-07-31 07:15:31 +0000

    update

---
+++ go.mod
@@ -0,0 +1,3 @@
+module clockbox
+
+go 1.20
---
+++ server.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func StartServer() {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+}
+
+func main() {
+	StartServer()
+}
